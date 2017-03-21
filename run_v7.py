09862b6aa098ae9671527a6bff0378f6442be9a3
Aaron Zhao <aaron@user-32-56.vpn.net.private.cam.ac.uk>
2017-03-21 08:33:28 +0800

    v7 updated

--- run_v7.py
+++ run_v7.py
@@ -12,14 +12,14 @@ def compute_file_name(pcov, pfc):
 acc_list = []
 count = 0
 pcov = [0., 0.]
-pfc = [99.9, 0.]
+pfc = [99., 0.]
 
 retrain = 0
 lr = 1e-4
 f_name = compute_file_name(pcov,pfc)
 
 while (count < 10):
-    pfc[0] = pfc[0] + 0.1
+    pfc[0] = pfc[0] + 1.
     if (retrain == 0):
         lr = 1e-4
     # prune
