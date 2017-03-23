b4ea56046b8722bd60dd20971a78bfb5d0e5c3ad
Aaron Zhao <aaron@user-32-56.vpn.net.private.cam.ac.uk>
2017-03-23 10:27:44 +0800

    without biases

--- run_v7_biases.py
+++ run_v7_biases.py
@@ -18,8 +18,8 @@ retrain = 0
 lr = 1e-4
 f_name = compute_file_name(pcov,pfc)
 pfc[1] = pfc[1] + 5.
-pcov[0] = pcov[0] + 10.
-pcov[1] = pcov[1] + 5.
+pcov[0] = pcov[0] + 5.
+pcov[1] = pcov[1] + 10.
 # pfc[0] = pfc[0] + 1.
 while (count < 10):
     # pfc[0] = pfc[0] + 10.
